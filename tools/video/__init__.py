a5558c2418303d2da5419a0a76fa0ee5fe404687
Ousmane <unicplaquiste@gmail.com>
2026-09-23 01:38:41 +0000

    feat(video): expose Agnes provider

--- tools/video/__init__.py
+++ tools/video/__init__.py
@@ -1,3 +1,4 @@
+from tools.video.agnes_provider import AgnesError, AgnesTask, AgnesVideoProvider
 from tools.video.ffmpeg_tool import FFmpegTool
 
-__all__ = ['FFmpegTool']
+__all__ = ["AgnesError", "AgnesTask", "AgnesVideoProvider", "FFmpegTool"]
